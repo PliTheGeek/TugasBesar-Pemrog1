915e26a86ded783dfab5f3ea96ff76480b5a279a
Fadhila Agil Permana <122727493+Spinoice72@users.noreply.github.com>
2023-06-20 11:31:08 +0700

    Delete Pass.py

--- Pass.py
+++
@@ -1,8 +0,0 @@
-def Keluar():
-    while True:
-        Pass = int(input("Masukan Password: "))
-        if (Pass) == 625789 :
-            print("Berhasil")
-            break
-        else:
-            print("Ulangi")
