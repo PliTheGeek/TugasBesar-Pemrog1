adf3e4c62530a95f91914bf7fa4d60f5a5e90004
Spinoice72 <fadhilaagil72@gmail.com>
2023-06-20 11:26:55 +0700

    raw code

---
+++ Pass.py
@@ -0,0 +1,8 @@
+def Keluar():
+    while True:
+        Pass = int(input("Masukan Password: "))
+        if (Pass) == 625789 :
+            print("Berhasil")
+            break
+        else:
+            print("Ulangi")
